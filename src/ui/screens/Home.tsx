56fb52432a52c7a904830e8b1a6f82ca73d37a85
Sam Henley <samueljhenley@gmail.com>
2024-11-18 09:28:07 -0700

    feat: add version to home

--- src/ui/screens/Home.tsx
+++ src/ui/screens/Home.tsx
@@ -3,6 +3,7 @@ import { View, StyleSheet } from 'react-native';
 import { Button, Text } from '@rneui/themed';
 import { style } from '../../styles';
 import { NavigationProp } from '@react-navigation/native';
+const packageJson = require('../../../package.json');
 
 interface HomeProps {
   navigation: NavigationProp<any>;
@@ -11,10 +12,15 @@ interface HomeProps {
 export function Home({ navigation }: HomeProps) {
   return (
     <View style={style.container}>
-      <Text h1 style={style.textCenter}>Scorekeeper</Text>
-      <Button title={'Start Game'} containerStyle={styles.buttonContainer} onPress={() => navigation.navigate('New Game')} />
-      <Button title={'Players'} containerStyle={styles.buttonContainer} onPress={() => navigation.navigate('Players')} />
-      <Button title={'Game History'} containerStyle={styles.buttonContainer} onPress={() => navigation.navigate('History')} />
+      <View style={styles.body}>
+        <Text h1 style={style.textCenter}>Scorekeeper</Text>
+        <Button title={'Start Game'} containerStyle={styles.buttonContainer} onPress={() => navigation.navigate('New Game')} />
+        <Button title={'Players'} containerStyle={styles.buttonContainer} onPress={() => navigation.navigate('Players')} />
+        <Button title={'Game History'} containerStyle={styles.buttonContainer} onPress={() => navigation.navigate('History')} />
+      </View>
+      <View style={styles.footer}>
+        <Text style={style.textCenter}>Version v{packageJson.version}</Text>
+      </View>
     </View>
   );
 }
@@ -24,5 +30,14 @@ const styles = StyleSheet.create({
     width: 200,
     marginHorizontal: 50,
     marginVertical: 10,
+  },
+  body: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  footer: {
+    flexShrink: 1,
+    padding: 10
   }
 });
