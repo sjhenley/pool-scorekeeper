ac446d30f051ff2922e0f7cf55043988fe22d974
Sam Henley <samueljhenley@gmail.com>
2024-11-20 22:17:46 -0700

    feat: theme mode toggle (#5)

--- src/ui/screens/Home.tsx
+++ src/ui/screens/Home.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Button, Text } from '@rneui/themed';
+import { Button, Text, Switch, useTheme, useThemeMode } from '@rneui/themed';
 import { useGlobalStyles } from '../../styles';
 import { NavigationProp, ParamListBase } from '@react-navigation/native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 const packageJson = require('../../../package.json');
 
 interface HomeProps {
@@ -11,9 +12,20 @@ interface HomeProps {
 
 export function Home({ navigation }: HomeProps) {
   const globalStyle = useGlobalStyles();
+  const themeMode = useThemeMode();
+  const theme = useTheme().theme;
 
   return (
     <View style={globalStyle.container}>
+      <View style={styles.viewModeToggleContainer}>
+        <Ionicons name="moon" size={27} color={theme.colors.primary} />
+        <Switch 
+          value={themeMode.mode === 'dark'}
+          onValueChange={(value) => themeMode.setMode(value ? 'dark' : 'light')}
+          style={styles.viewModeToggleSwitch}
+          trackColor={{ false: theme.colors.primary, true: theme.colors.primary }}
+        />
+      </View>
       <View style={styles.body}>
         <Text style={styles.title}>Scorekeeper</Text>
         <Button title={'Start Game'} buttonStyle={globalStyle.buttonLarge} titleStyle={globalStyle.buttonPrimaryText} onPress={() => navigation.navigate('New Game')} />
@@ -40,5 +52,24 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 48
+  },
+  viewModeToggleContainer: {
+    position: 'absolute',
+    top: 30,
+    right: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10
+  },
+  viewModeToggleIcon: {
+
+  },
+  viewModeToggleSwitch: {
+    transform: [
+      { scaleX:  1.5 }, 
+      { scaleY:  1.5 }
+    ]
   }
 });
