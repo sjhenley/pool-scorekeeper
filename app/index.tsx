05299c737661ca97dbf70f0d68c4ba224d232aee
Sam Henley <samueljhenley@gmail.com>
2025-09-23 10:24:18 -0600

    main menu gradient

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,7 @@ import { colorScheme } from 'nativewind';
 import { useState } from 'react';
 import { Text, View } from 'react-native';
 import { Button } from '@/components/Button';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Index() {
   const router = useRouter();
@@ -14,14 +15,21 @@ export default function Index() {
   };
 
   return (
-    <View className="bg-background-200 dark:bg-background-900 flex-1 justify-center items-center gap-10 p-5">
+    <LinearGradient
+      colors={
+        currentTheme === 'dark'
+          ? ['#060e23', '#3a5dd9']
+          : ['#4788C4', '#A8A8A8']
+      }
+      start={{ x: 0.5, y: 0.4 }}
+      end={{ x: 0.5, y: 1 }}
+      className="flex-1 justify-center items-center gap-10 p-5">
       <Text className="text-6xl font-sans font-bold text-text-900 dark:text-text-100">Scorekeeper</Text>
       <Button primary onPress={() => router.navigate('/storybook')} label="Go to Storybook" />
       <Button
         label={`Switch to ${currentTheme === 'dark' ? 'light' : 'dark'} mode`}
         onPress={() => setColorScheme(currentTheme === 'dark' ? 'light' : 'dark')}
       />
-      <View className="bg-green-500 text-green-500 outline-purple-600" />
-    </View>
+    </LinearGradient>
   );
 }
