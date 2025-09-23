12cefd7c8a9e43d4640f19073eaccdc49dab8535
Sam Henley <samueljhenley@gmail.com>
2025-09-23 10:24:28 -0600

    card component

---
+++ .rnstorybook/stories/Card.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react-native';
+import { colorScheme } from 'nativewind';
+import { useState } from 'react';
+import { Button as NativeButton, View } from 'react-native';
+import { fn } from 'storybook/test';
+import { Card } from '../../components/Card';
+
+const meta = {
+  title: 'Card',
+  component: Card,
+  decorators: [
+    (Story) => {
+      const [currentTheme, setCurrentTheme] = useState<'light' | 'dark'>('light');
+
+      const setColorScheme = (scheme: 'light' | 'dark') => {
+        colorScheme.set(scheme);
+        setCurrentTheme(scheme);
+      };
+      return (
+        <View className="flex-1 items-center justify-center bg-background-300 dark:bg-background-900 p-5 gap-10">
+          <Story />
+          <NativeButton
+            title={`Switch to ${currentTheme === 'dark' ? 'light' : 'dark'} mode`}
+            onPress={() => setColorScheme(currentTheme === 'dark' ? 'light' : 'dark')}
+          />
+        </View>
+      );
+    }
+  ],
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  args: {
+    onPress: fn(),
+    image: require('../../assets/balls/8.png')
+  }
+  // Use `fn` to spy on the onPress arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#story-args
+} satisfies Meta<typeof Card>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    title: '8 Ball',
+    description: 'Shoot all your group balls then pocket the 8 ball',
+    cta: 'Play Now',
+    disabled: false
+  }
+};
+
