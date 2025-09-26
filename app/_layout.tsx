079def509fdf6e58f0b71bc33f5781a0f5907491
Sam Henley <samueljhenley@gmail.com>
2025-09-25 18:45:32 -0600

    screen headers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,29 @@
 import { Stack } from 'expo-router';
 import '../global.css';
+import { useColorScheme } from 'nativewind';
 
 export default function RootLayout() {
-  return <Stack
-    screenOptions={{ headerShown: false }}
-  />;
+  const colorScheme = useColorScheme();
+  return (
+    <Stack
+      screenOptions={{
+        headerShadowVisible: true,
+        headerStyle: {
+          backgroundColor: colorScheme.colorScheme === 'dark' ? '#a2a5c3' : '#50547c'
+        },
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          fontSize: 28,
+          fontWeight: 'bold',
+          color: colorScheme.colorScheme === 'dark' ? '#1e1e2f' : '#e4e4e6',
+          fontFamily: 'sans'
+        }
+      }}
+    >
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="player-list" options={{ title: 'Player List' }} />
+      <Stack.Screen name="settings" options={{ title: 'Settings' }} />
+      <Stack.Screen name="storybook" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
